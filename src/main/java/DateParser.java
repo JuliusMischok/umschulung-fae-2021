07aee50c0061c6209d317f3e7a61cdc2175fa211
Julius Mischok <julius.mischok@mischok.de>
2021-10-21 09:10:00 +0200

    add further examples

--- src/main/java/DateParser.java
+++ src/main/java/DateParser.java
@@ -1,9 +1,48 @@
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeParseException;
+import java.util.List;
+import java.util.Locale;
 import java.util.Objects;
 
 public class DateParser {
+
+    public static void main(String[] args) {
+        String hello = "Hello world Hello wo";
+
+        if (hello.length() >= 20) {
+            System.out.println("#" + hello.substring(20) + "#");
+        }
+
+        String emailFromDatabase = "julius.mischok@mischok.de";
+        String emailInput = " JULIUS.Mischok@mischok.de ";
+
+        System.out.println("#" + emailInput.toUpperCase(Locale.ROOT) + "#");
+
+        if (emailFromDatabase.equalsIgnoreCase(emailInput.trim())) {
+            System.out.println("emails match");
+        } else {
+            System.out.println("emails does not match");
+        }
+
+        System.out.println("===========================================");
+
+        System.out.println(emailInput.strip().toUpperCase());
+
+        char fourthChar = emailInput.strip().toUpperCase().charAt(3);
+
+        System.out.println(fourthChar);
+        System.out.println((int)fourthChar);
+
+        System.out.println("===========================================");
+
+        List<String> ipComponents = List.of("122", "12", "14", "1");
+
+        System.out.println(ipComponents.get(0) + "." + ipComponents.get(1) + "." + ipComponents.get(2) + "." + ipComponents.get(3));
+
+        System.out.println(String.join(".", ipComponents));
+        System.out.println(String.join(".", "123", "13", "15", "122"));
+    }
 
     public static int parseDay(String input) {
         if (input == null) {
